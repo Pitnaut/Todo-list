733775f06802554bf4a7874c5ffe2055e2a19064
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-08-08 11:09:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1 className="title">To Do List</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
