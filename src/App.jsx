8482d67c8a56af34802ca4c46292f40db38a4a99
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-08-08 13:19:26 +0200

    Setup of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import { addTask, deleteTask, markDone, cancelUpdate, changeTask, updateTask } from './Helper/helper';
 import './App.css'
+
 function App() {
+
+  const {toDo, setToDo} = useState([
+    {id: 1, title: "Task 1", status: false},
+    {id: 2, title: "Task 2", status: false}
+  ]);
+
+  const {newTask, setnewTask} = useState('');
+
+  const {updateData, setUpdateData} = useState('');
 
   return (
     <>
       <header>
-        <h1 className="title">To Do List</h1>
+        <h1 className="title">To-Do-List</h1>
       </header>
     </>
   )
