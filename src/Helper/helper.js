8d4bf01117625155d375a068761f9b80babec0da
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-08-08 13:17:32 +0200

    add helper funcions

---
+++ src/Helper/helper.js
@@ -0,0 +1,31 @@
+//Add Task
+const addTask = () => {
+
+}
+
+//Delete Task
+const deleteTask = (id) => {
+
+}
+
+//Mark task as completed
+const markDone = (id) => {
+
+}
+
+//Cancel update
+const cancelUpdate = () => {
+
+}
+
+//Change task for Update
+const changeTask = (e) => {
+
+}
+
+//Update task
+const updateTask = () => {
+
+}
+
+export { addTask, deleteTask, markDone, cancelUpdate, changeTask, updateTask }
